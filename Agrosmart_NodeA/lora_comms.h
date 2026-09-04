310111c45e717da1ce952a1ab475e880f2a9df3b
PRATEEK SHEOKAND <166098220+Prateekandc@users.noreply.github.com>
2026-09-04 22:40:01 +0530

    Delete Agrosmart_NodeA directory

--- Agrosmart_NodeA/lora_comms.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "structs.h"
-
-bool initLora();
-bool transmitPacket(const MasterSensorRecord &record);
-bool loraIsReady();
--- Agrosmart_NodeA/modbus_core.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "structs.h"
-
-void initModbus();
-bool readSoilSensor(MasterSensorRecord &r);
